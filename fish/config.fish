67fbfb26d84618f2795a27d13b13f4fab4e39968
Jordan lacopo <jordanlacopo@gmail.com>
2023-02-15 07:55:00 -0500

    added new configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
