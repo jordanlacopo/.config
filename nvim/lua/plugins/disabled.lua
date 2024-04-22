80e07934a7600d6142f49601aafb0cdc6694ef6b
Jordan Lacopo <jordanlacopo@gmail.com>
2024-04-22 13:46:55 -0400

    Added wezterm configs

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,3 +1,13 @@
+-- List of disabled plugins
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
+  {
+    "folke/noice.nvim",
+    opts = {
+      notify = {
+        enabled = false,
+      },
+    },
+  },
+  { "rcarriga/nvim-notify", enabled = false },
 }
